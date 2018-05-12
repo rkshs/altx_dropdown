ba99433b01e50a38f5f657fa100c2bfc77922c99
SAMARA\Grigorii_Nebritov <gnebritov@oneviewhealthcare.com>
2018-05-12 19:08:23 +0400

    boiler plate + styl

---
+++ dropdown/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Input from './Input';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Input />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ dropdown/src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div >
+            <input className="input__field" type="text"/>
+        </div>
+    );
+};
+
+export default Input;
